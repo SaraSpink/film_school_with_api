2703c5ad603dc912820d0b8f3de43e10221079e5
Rebecca McIntosh and Sara Spink <sara@>
2017-11-29 15:02:32 -0800

    create separate authorizations for users and admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # helper_method :current_user
-  #
-  # def current_user
-  #   if session[:user_id]
-  #     @current_user ||= User.find(session[:user_id])
-  #   end
-  # end
+
+
+  def authorize
+    if !current_user
+      flash[:alert] = "Please sign in to visit this page."
+      redirect_to '/'
+    end
+  end
+
+  def authorize_admin
+
+    if current_user.admin === true
+      new_location_path
+    else
+      flash[:alert] = "Only an admin can visit this page."
+      redirect_to '/'
+    end
+  end
+
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,11 @@
 class LocationsController < ApplicationController
+  before_action :authorize, only: [:index]
+  before_action :authorize_admin, only: [:new]
   def index
+    @locations = Location.all
+  end
+
+  def new
+    @location = Location.new
   end
 end
