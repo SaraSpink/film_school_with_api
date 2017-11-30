29622c647e706668ff0890e6124811d46dfc6016
Rebecca McIntosh and Sara Spink <rebecca@>
2017-11-30 11:53:29 -0800

    build out our location model with CRUD

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,7 +5,46 @@ class LocationsController < ApplicationController
     @locations = Location.all
   end
 
+  def show
+    @location =Location.find(params[:id])
+  end
+
   def new
     @location = Location.new
   end
+
+  def create
+    @location = Location.new(location_params)
+      if @location.save
+        flash[:notice] = "Location Successfully Added"
+        redirect_to locations_path
+      else
+        render :new
+      end
+    end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update(location_params)
+      redirect_to locations_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    @location.destroy
+    redirect_to locations_path
+  end
+
+  private
+    def location_params
+      params.require(:location).permit(:name, :avatar)
+    end
+
 end
