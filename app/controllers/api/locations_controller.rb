8e3ab75e04af53c9a99b672aa2a1da8d72ef0dc6
rebecca mcintosh and sara spink <rebecca@>
2017-12-13 15:47:10 -0800

    add correct http error messages to our json responses

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -13,19 +13,41 @@ class Api::LocationsController < ApplicationController
   def create
     location = Location.new(location_params)
     if location.save
-      render json: {
-        status: 200,
+      render status: 201, json: {
         mesage: "Successfully created location!",
         location: location
       }.to_json
     else
-      render json: {
-        status: 500,
+      render status: 422, json: {
         errors: location.errors,
         mesage: "You done fucked up."
       }.to_json
     end
   end
+
+  def update
+    location = Location.find(params[:id])
+    if location.update(location_params)
+      render status: 200, json: {
+        message: "you Successfully updated this location",
+        location: location
+      }.to_json
+    else
+      render status: 422, json: {
+        message: "The location could not be updated, go back to code school!",
+        location: location
+      }.to_json
+    end
+  end
+
+  def destroy
+    location = Location.find(params[:id])
+    location.destroy
+    render status: 200, json: {
+      message: "You deleted the location! Hope you meant to do that."
+    }.to_json
+  end
+
 
   private
   def location_params
